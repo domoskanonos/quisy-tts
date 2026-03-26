c2fbdcff0d5f1633c490ce293eb705b9cfba4f7c
Dominik Bruhn <domoskanonos@googlemail.com>
2026-03-26 11:17:08 +0100

    test(playwright): use correct base URL /ui for dev server and tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:4200',
+    // App is served from a sub-path (`baseHref` set to "/ui/" in angular.json)
+    baseURL: 'http://localhost:4200/ui',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+  },
 });
